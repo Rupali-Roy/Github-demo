e883a32ba25885b2713593906baa1fdcbd96f916
RUPRAJ ROY <RUPRAJ ROY@RUPRAJ.attlocal.net>
2024-11-08 18:30:55 -0600

    commit name

---
+++ src/test/java/githubDemo/githubDemo/printName.java
@@ -0,0 +1,11 @@
+package githubDemo.githubDemo;
+
+public class printName {
+
+	public static void main(String[] args) {
+		System.out.println("MY NAME IS RUPALI");
+		// TODO Auto-generated method stub
+
+	}
+
+}
